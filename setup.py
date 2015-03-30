61602d08b551b4778e85ef899dc863743c13b183
Denis Vilar <denisvilar@Deniss-MacBook-Pro-2.local>
2015-03-30 15:42:40 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='AlchemyAPI',
+      version='1.1',
+      description='AlchemyAPI Python bindings',
+      author='AlchemyAPI',
+      url='https://github.com/AlchemyAPI/alchemyapi_python',
+      py_modules=['alchemyapi'])
